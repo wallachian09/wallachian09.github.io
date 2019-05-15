596910ea84d19ae307fbff8b009e132e02df7c68
wallachian09 <masterkcoreh54@gmail.com>
2019-05-15 21:39:25 +0300

    commit meat

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/*var rightMenu = document.getElementById('leftMenu');
+var menuRight = -65;
+function menuSlide () {
+  menuRight += 65;
+  if (menuRight > 0) {
+    menuRight = -65;
+  }
+  rightMenu.style.right = menuRight + '%';
+}*/
---
+++ js/script.js
@@ -0,0 +1,101 @@
+var rightMenu = document.getElementById('leftMenu');
+var menuRight = -65;
+
+document.getElementById('left').onclick = sliderLeft;
+var slider = document.getElementById('slider_wrapper');
+var left = 0;
+
+var main, about, portfolio, blog, content;
+about = document.getElementById('about');
+portfolio = document.getElementById('portfolio');
+blog = document.getElementById('blog');
+content = document.getElementById('content');
+main = document.getElementById('home');
+showHome ();
+//SLIDER
+function sliderRight () {
+  left = left - 100;
+  if (left < -100) {
+    left = 0;
+  }
+  slider.style.left = left + '%';
+}
+
+function sliderLeft () {
+  slider.style.left = 0 + '%';
+}
+document.getElementById('slider_wrapper').onclick = sliderRight;
+
+//MENU
+function menuSlide () {
+  menuRight += 65;
+  if (menuRight > 0) {
+    menuRight = -65;
+  }
+  rightMenu.style.right = menuRight + '%';
+}
+
+function showHome () {
+  $(main).show();
+  $(about).hide();
+  $(portfolio).hide();
+  $(blog).hide();
+  $(content).hide();
+}
+
+function showAbout () {
+  $(main).hide();
+  $(about).show();
+  $(portfolio).hide();
+  $(blog).hide();
+  $(content).hide();
+}
+
+function showPortfolio () {
+  $(main).hide();
+  $(about).hide();
+  $(portfolio).show();
+  $(blog).hide();
+  $(content).hide();
+}
+
+function showBlog () {
+  $(main).hide();
+  $(about).hide();
+  $(portfolio).hide();
+  $(blog).show();
+  $(content).hide();
+}
+
+function showContent () {
+  $(main).hide();
+  $(about).hide();
+  $(portfolio).hide();
+  $(blog).hide();
+  $(content).show();
+}
+
+//GALLERY
+function showSC () {
+  $(".second_col").show();
+  $(".third_col").show();
+}
+
+
+
+
+
+
+
+/*if(window.matchMedia('(min-width: 315px)').matches){
+  $(".second_col").hide();
+  $(".third_col").hide();
+} else if (window.matchMedia('(max-width: 575px)').matches){
+  $(".second_col").hide();
+  $(".third_col").hide();
+}
+
+function showGallery () {
+  var cols = [$(".second_col"), $(".third_col")]
+  cols[0].show;
+}*/
