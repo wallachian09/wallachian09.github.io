07eaabc1ea4ccff8b3c31bce643ad8251f334c34
wallachian09 <masterkcoreh54@gmail.com>
2019-06-23 16:19:57 +0300

    Patch

--- Bicycle/js/script.js
+++ Bicycle/js/script.js
@@ -67,6 +67,10 @@ $(document).ready(function () {
         classToAdd: 'visible animated fadeIn', 
         offset: 200
     });
+    $('#tweets').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn', 
+        offset: 200
+    });
     $('.first').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated bounceInRight', 
         offset: 200
@@ -80,6 +84,10 @@ $(document).ready(function () {
         offset: 200
     });
     $('.shop2').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated bounceIn', 
+        offset: 200
+    });
+    $('.shop3').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated bounceIn', 
         offset: 200
     });
